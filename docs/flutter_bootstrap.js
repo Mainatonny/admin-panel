44468e1990a699e31f52146c26dca867901fc98e
Mainatonny <antony.macharia21@dkut.students.dkut.ac.ke>
2025-03-14 20:59:18 +0300

    Fixed GitHub Pages blank page issue

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "138269192"
+    serviceWorkerVersion: "2330347869"
   }
 });
